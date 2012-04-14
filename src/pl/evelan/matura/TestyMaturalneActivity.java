d81fcbb6ea7dd152b318f4d1ea30790989004b78
Jakub Pomykala <evelan3094@gmail.com>
2012-04-14 16:55:27 +0200

    first commit

---
+++ src/pl/evelan/matura/TestyMaturalneActivity.java
@@ -0,0 +1,13 @@
+package pl.evelan.matura;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TestyMaturalneActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
